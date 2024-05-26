b70e39b6835d9dc4254c490b37063a2e5f05e9cd
thomas <18520168+yaythomas@users.noreply.github.com>
2024-05-26 11:46:59 -0700

    add searchbox and algolia config

---
+++ assets/js/search.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    docsearch({
+        appId: "CY7A57K6PQ",
+        apiKey: "a3a884f4132d74e39330b8d362a13a3f",
+        indexName: "pypyr",
+        container: '#docsearch',
+        debug: false
+    });
+});
